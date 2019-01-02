09aaf8e55c7a393be972fc81600c5451162531c3
Abjec <maxime.radomski@gmail.com>
2019-01-02 08:14:27 +0100

    ScoreBackground and Font

---
+++ Assets/Scripts/ScoreBackgroundBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBackgroundBehavior : MonoBehaviour
+{
+	public SpriteRenderer SpriteRenderer;
+	public Sprite SpriteWin;
+	public Sprite SpriteLoose;
+
+	public void Win()
+	{
+		SpriteRenderer.sprite = SpriteWin;
+	}
+
+	public void Loose()
+	{
+		SpriteRenderer.sprite = SpriteLoose;
+	}
+}
