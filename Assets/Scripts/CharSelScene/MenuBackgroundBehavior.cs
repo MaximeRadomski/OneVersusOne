34a3acdbdf4c825cdfa83a8c5045c53d554a15dd
Maxime Radomski <maxime.radomski@apollossc.com>
2019-01-25 17:41:45 +0100

    Scrolling Fucking Background

---
+++ Assets/Scripts/CharSelScene/MenuBackgroundBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBackgroundBehavior : MonoBehaviour
+{
+	public float ScrollSpeed;
+	public MeshRenderer Renderer;
+
+	void Start()
+	{
+		Renderer.sortingLayerName = "Background";
+	}
+
+	void Update ()
+	{
+		float offset = Time.time * ScrollSpeed;
+		Renderer.material.mainTextureOffset = new Vector2(offset, offset/2);
+	}
+}
