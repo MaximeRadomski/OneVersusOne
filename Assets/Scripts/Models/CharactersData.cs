63a0c7494cc314f09121e2a2105d91cba63f029d
Maxime Radomski <maxime.radomski@apollossc.com>
2019-02-05 17:32:15 +0100

    Fixed BlueDiscClockwise animation + Creating Characters and Map Data

---
+++ Assets/Scripts/Models/CharactersData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharactersData
+{
+	public List<Character> Characters;
+
+	public class Character
+	{
+		public string Name;
+		public string Super;
+	}
+
+	public CharactersData()
+	{
+		Characters = new List<Character>
+		{
+			new Character{Name = "MARY POPPIN", Super = "GOLDEN GATE"},
+			new Character{Name = "HUGUETTE BAGUETTE", Super = "OMELETTE DU FROMAGE"},
+			new Character{Name = "GUNTHER VON MARMELADE", Super = "BLITZKRIEG"},
+			new Character{Name = "CODY BEAVER", Super = "SORRY"},
+			new Character{Name = "JOHN DUFF", Super = "SNIPER RIFLE"},
+			new Character{Name = "DZAK ITSAN", Super = "OMAE WA MOU SHINDEIRU"}
+		};
+	}
+}
---
+++ Assets/Scripts/Models/MapsData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapsData
+{
+	public List<Map> Maps;
+
+	public class Map
+	{
+		public string Name;
+		public string Country;
+		public string City;
+		public string Effect;
+	}
+
+	public MapsData()
+	{
+		Maps = new List<Map>
+		{
+			new Map {
+				Name = "RENÉ COTY STADIUM",
+				Country = "FRANCE",
+				City = "BISCARROSSE",
+				Effect = "NONE"},
+			new Map {
+				Name = "X-WIND",
+				Country = "UNITED KINGDOM",
+				City = "BIRMINGHAM",
+				Effect = "FASTER DISC"}
+		};
+	}
+}
