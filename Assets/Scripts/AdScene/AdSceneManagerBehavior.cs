037d3c7252786e1c30c9b46dea0776360cdc5ec6
Maxime Radomski <maxime.radomski@gmail.com>
2019-06-19 13:58:58 +0200

    New ads assets

--- Assets/Scripts/AdScene/AdSceneManagerBehavior.cs
+++ Assets/Scripts/AdScene/AdSceneManagerBehavior.cs
@@ -4,18 +4,50 @@ using UnityEngine;
 
 public class AdSceneManagerBehavior : MonoBehaviour
 {
+	private GameObject _gameIcon, _gameName, _gameImage, _gameDescription;
 	private GameObject _leaveButton;
+
+	private int _adId;
 
 	void Start ()
 	{
+		_gameIcon = GameObject.Find ("GameIcon");
+		_gameName = GameObject.Find ("GameName");
+		_gameImage = GameObject.Find ("GameImage");
+		_gameDescription = GameObject.Find ("GameDescriptionText");
 		_leaveButton = GameObject.Find ("LeaveButton");
 
 		StartCoroutine (InitiateLeft());
+		SetAd ();
+	}
+
+	private void SetAd()
+	{
+		_adId = PlayerPrefs.GetInt ("CurrentAd", 0);
+		PlayerPrefs.SetInt ("CurrentAd", _adId + 1 < AdsData.GameAds.Count ? _adId + 1 : 0);
+
+		var folderName = AdsData.GameAds [_adId].Name.Replace (" ", "");
+		_gameIcon.GetComponent<UnityEngine.UI.Image>().sprite = Resources.Load<Sprite> ("Ads/" + folderName + "/GameIcon");
+		_gameName.GetComponent<UnityEngine.UI.Text> ().text = AdsData.GameAds [_adId].Name;
+		_gameImage.GetComponent<UnityEngine.UI.Image>().sprite = Resources.Load<Sprite> ("Ads/" + folderName + "/GameImage");
+		_gameDescription.GetComponent<UnityEngine.UI.Text> ().text = AdsData.GameAds [_adId].Description;
 	}
 
 	private IEnumerator InitiateLeft()
 	{
 		yield return new WaitForSeconds(1.0f);
 		_leaveButton.GetComponent<Animator> ().Play ("LeftOut-RightMiddle");
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			//We transform the touch position into word space from screen space and store it.
+			var touchPosWorld = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			if (touchPosWorld.y > -2.0f)
+				Application.OpenURL ("market://details?id=" + AdsData.GameAds [_adId].UrlId);
+			else if (touchPosWorld.x > 1.0f)
+				SetAd ();
+		}
 	}
 }
---
+++ Assets/Scripts/Models/AdsData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdsData : MonoBehaviour
+{
+	public static List<GameAd> GameAds = new List<GameAd>
+	{
+		new GameAd {
+			Name = "Horizon Chase",
+			Description = "You like old arcade racing games ?\nThen this one is made for you !\nEasy to learn/hard to master gameplay, Gorgeous musics and a real challenge await you with horizon chase",
+			UrlId = "com.aquiris.horizonchase"},
+		new GameAd {
+			Name = "Downwell",
+			Description = "A tough roguelike with a simple yet effective gameplay.\nRequires some advanced skills to complete it.\nHas a good replayability and a fine pixel art.",
+			UrlId = "com.devolver.downwell"},
+		new GameAd {
+			Name = "Pixel Dungeon",
+			Description = "An homage to old roguelike games.\nVery difficult and a little luck-based.\nAs its creator says : \"Anyway, you will die often. You are warned!\"",
+			UrlId = "com.watabou.pixeldungeon"}
+	};
+
+	public class GameAd
+	{
+		public string Name;
+		public string Description;
+		public string UrlId;
+	}
+}
